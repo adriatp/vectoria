1fe43784a521ea85bc30bc1302bfe9e718c7010e
adriatp <tarradas.adria@gmail.com>
2024-03-22 11:09:28 +0100

    Update project structure and add ipynb

--- src/vectoria/vectoria.py
+++ src/vectoria/vectoria.py
@@ -6,17 +6,22 @@ from .shared_variables import *
 
 args = parse_arguments()
 
-def export_to_svg(vectors):
+def export_to_svg(vectors, id_svg='svg', class_svg='svg'):
     dwg = svgwrite.Drawing(args.output_path, profile='tiny')
-    for vector in vectors:
-        dwg.add(dwg.line(vector[0], vector[1], stroke=svgwrite.rgb(10, 10, 16, '%')))
+
+    # Define los puntos del polígono irregular
+    points = [(100, 100), (200, 100), (200, 200), (150, 150), (100, 200)]
+
+    # Crea el polígono y lo añade al SVG
+    dwg.add(dwg.polygon(points=points))
+
+    # Guarda el SVG
     dwg.save()
 
 def image_to_vector(image_array):
     vectors = []
     # Get the dimensions of the image
     height, width, _ = image_array.shape
-    
     # Iterate over each pixel in the image
     for y in range(height):
         for x in range(width):
@@ -31,12 +36,17 @@ def image_to_vector(image_array):
     
     return vectors
 
+def image_to_greyscale(image_array):
+    
+
 def main():
     # Open the image from args
     try:
         image = Image.open(args.image_path)
-        image_array = np.array(image)
-        vectors = image_to_vector(image_array)
+        # image_array = np.array(image)
+        
+        # pdb.set_trace()
+        vectors=[]
         export_to_svg(vectors)
     except IOError:
         print(f"Error: Can't open image at {args.image_path}. Please check the file path and try again.")
